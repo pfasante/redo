31a946fa6b86af49713b825952b08247c2075261
Friedrich Wiemer <friedrich.wiemer@rub.de>
2013-09-28 00:25:21 +0200

    episode 6

--- redo.hs
+++ redo.hs
@@ -1,11 +1,21 @@
+{-# LANGUAGE StandaloneDeriving #-}
+
 import Control.Monad (filterM, liftM)
+import Data.Map.Lazy (adjust, insert, fromList, toList)
 import Data.Maybe (listToMaybe)
+import Debug.Trace (traceShow)
 import System.Directory (doesFileExist, renameFile, removeFile)
-import System.Environment (getArgs)
+import System.Environment (getArgs, getEnvironment)
 import System.Exit (ExitCode(..))
 import System.FilePath (hasExtension, replaceBaseName, takeBaseName)
 import System.IO (hPutStrLn, stderr)
-import System.Process (createProcess, shell, waitForProcess)
+import System.Process (createProcess, shell, waitForProcess, CreateProcess(..), StdStream(..), CmdSpec(..))
+
+traceShow' arg = traceShow arg arg
+
+deriving instance Show CreateProcess
+deriving instance Show StdStream
+deriving instance Show CmdSpec
 
 main :: IO ()
 main = mapM_ redo =<< getArgs
@@ -14,7 +24,9 @@ redo :: String -> IO ()
 redo target = maybe printMissing redo'=<< redoPath target
  where redo' :: FilePath -> IO ()
        redo' path = do
-         (_,_,_,ph) <- createProcess . shell $ cmd path 
+         oldEnv <- getEnvironment
+         let newEnv = toList $ adjust (++ ":.") "PATH"  $ insert "REDO_TARGET" target $ fromList oldEnv
+         (_,_,_,ph) <- createProcess $ traceShow' $ (shell $ cmd path) {env = Just newEnv}
          exit <- waitForProcess ph
          case exit of
            ExitSuccess -> do renameFile tmp target
@@ -22,7 +34,7 @@ redo target = maybe printMissing redo'=<< redoPath target
                                   removeFile tmp
        printMissing = error $ "No .do file found for target '" ++ target ++ "'"
        tmp = target ++ "---redoing"
-       cmd path = unwords ["sh", path, "0", takeBaseName target, tmp, ">", tmp]
+       cmd path = traceShow' $ unwords ["sh", path, "0", takeBaseName target, tmp, ">", tmp]
 
 redoPath :: FilePath -> IO (Maybe FilePath)
 redoPath target = listToMaybe `liftM` filterM doesFileExist candidates 
